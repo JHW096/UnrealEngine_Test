0cc5482726af14e65e935c9eedb361c1c72e2050
Jeon <dbs_dmswl92@naver.com>
2023-03-19 06:02:52 +0900

    add- Pawn Movment

---
+++ TestUnreal/Source/TestUnreal/MyGameModeBase.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+#include "MyPawn.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+	//UCLASS()에 등록된 Class, UCLASS에 등록된 Class는 StaticClass함수 포함.//Static객체를 받아 온다.
+	DefaultPawnClass = AMyPawn::StaticClass();
+}
---
+++ TestUnreal/Source/TestUnreal/MyPawn.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "MyPawn.generated.h"
+
+
+UCLASS()
+class TESTUNREAL_API AMyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMyPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	//Actor와 가장 큰 차이. 입력을 받는 함수
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+//MyFunction
+public:
+	void UpDown(float Value);
+	void LeftRight(float Value);
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere)
+	class UFloatingPawnMovement* Movement;
+	
+};
