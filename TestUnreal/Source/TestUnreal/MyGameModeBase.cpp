33f2fb0752ac76ec09fcd1d5028a15331e1b10ca
Jeon <dbs_dmswl92@naver.com>
2023-03-20 09:00:35 +0900

    BluePrintClass

--- TestUnreal/Source/TestUnreal/MyGameModeBase.cpp
+++ TestUnreal/Source/TestUnreal/MyGameModeBase.cpp
@@ -8,4 +8,12 @@ AMyGameModeBase::AMyGameModeBase()
 {
 	//UCLASS()에 등록된 Class, UCLASS에 등록된 Class는 StaticClass함수 포함.//Static객체를 받아 온다.
 	DefaultPawnClass = AMyCharacter::StaticClass();
+
+	static ConstructorHelpers::FClassFinder<ACharacter> BP_Char(
+		TEXT("Blueprint'/Game/BluePrints/BP_MyCharacter.BP_MyCharacter_C'"));
+
+	if (BP_Char.Succeeded())
+	{
+		DefaultPawnClass = BP_Char.Class;
+	}
 }
